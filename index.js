148c97820696fb13abfa1e69ae55b8a9db231642
Gabrielviei <164223054+Gabrielviei@users.noreply.github.com>
2024-11-16 01:27:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+    const nome = document.getElementById("nome");
+    const aniversario = document.getElementById("aniversario");
+    const corFavorita = document.getElementById("cor-favorita");
+    const comeco = document.getElementById("comeco");
+    const nomeCerto ="Gabriel Martins Vieira";
+    const aniversarioCerto ="2025-05-18";
+    const corFavoritaCerto ="Rosa";
+    const comecoCerto ="2024-03-23";
+
+    function teste(){
+        const nomeValor = nome.value;
+        const aniversarioValor = aniversario.value;
+        const corFavoritaValor = corFavorita.value;
+        const comecoValor = comeco.value;
+    if(nomeValor==nomeCerto && aniversarioValor==aniversarioCerto 
+    && corFavoritaValor==corFavoritaCerto && comecoValor==comecoCerto){
+        window.location.href='lembrancas.html';
+    }
+    else{
+        alert("errou");
+    }
+    }
